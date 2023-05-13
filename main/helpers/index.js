dc0dc03e38dc08c309146537cb776fcf69104e97
kc-1 <84696121+kelvinkaifungchan@users.noreply.github.com>
2023-05-13 12:18:21 +0100

    restart

--- main/helpers/index.js
+++
@@ -1,9 +0,0 @@
-import createWindow from './create-window';
-import getAllFiles from './getAllFiles';
-import getRecipeData from './getRecipeData';
-
-export {
-  createWindow,
-  getAllFiles,
-  getRecipeData
-};
